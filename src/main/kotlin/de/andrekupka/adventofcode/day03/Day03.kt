a3272b3520472b435addf404e60aa60f907afdd9
Andre Kupka <me-github@andrekupka.de>
2021-12-03 19:41:15 +0100

    solution for day 3 part 2

--- src/main/kotlin/de/andrekupka/adventofcode/day03/Day03.kt
+++ src/main/kotlin/de/andrekupka/adventofcode/day03/Day03.kt
@@ -19,15 +19,9 @@ fun Int.invertWithBitSize(bitSize: Int): Int {
     return this.inv() and mask
 }
 
-fun main() {
-    val numberStrings = readResourceLinesNotBlank("/day03/input")
-
-    val bitSize = numberStrings.map { it.length }.distinct().single()
-
-    val numbers = numberStrings.map { it.toInt(radix = 2) }
-
+fun determinePowerConsumption(report: List<Int>, bitSize: Int) {
     val bitsOfIndices = (0 until bitSize).map { bitIndex ->
-        numbers.map { it.bitAtIndex(bitIndex) }
+        report.map { it.bitAtIndex(bitIndex) }
     }
 
     val mostCommonBits = bitsOfIndices.map { bits -> bits.count { it } > bits.size / 2 }
@@ -35,9 +29,53 @@ fun main() {
     val gammaRate = mostCommonBits.toIntFromBinary()
     val epsilonRate = gammaRate.invertWithBitSize(bitSize)
 
-    val product = gammaRate * epsilonRate
+    val powerConsumption = gammaRate * epsilonRate
 
     println("gamma rate is $gammaRate")
     println("epsilon rate is $epsilonRate")
-    println("product of both is $product")
+    println("power consumption is $powerConsumption")
+}
+
+fun List<Int>.mostCommonBitAtIndex(index: Int): Boolean {
+    val onesCount = map { it.bitAtIndex(index) }.count { it }
+    return onesCount >= (size - onesCount)
+}
+
+fun List<Int>.filterByIndexedBits(bitSize: Int, bitSelector: (List<Int>, Int) -> Boolean) =
+    (bitSize-1 downTo  0).fold(this) { currentNumbers, bitIndex ->
+        println(currentNumbers.map { it.toString(radix=2) })
+        if (currentNumbers.size == 1) {
+            currentNumbers
+        } else {
+            val bit = bitSelector(currentNumbers, bitIndex)
+            currentNumbers.filter { it.bitAtIndex(bitIndex) == bit }
+        }
+    }.single()
+
+
+fun determineLifeSupportRating(report: List<Int>, bitSize: Int) {
+    val oxygenGeneratorRating = report.filterByIndexedBits(bitSize) { currentNumbers, bitIndex ->
+        currentNumbers.mostCommonBitAtIndex(bitIndex)
+    }
+
+    val co2ScrubberRating = report.filterByIndexedBits(bitSize) { currentNumbers, bitIndex ->
+        !currentNumbers.mostCommonBitAtIndex(bitIndex)
+    }
+
+    val lifeSupportRating = oxygenGeneratorRating * co2ScrubberRating
+
+    println("oxygen generator rating is $oxygenGeneratorRating")
+    println("co2 scrubber rating is $co2ScrubberRating")
+    println("life support rating is $lifeSupportRating")
+}
+
+fun main() {
+    val reportStrings = readResourceLinesNotBlank("/day03/input")
+
+    val bitSize = reportStrings.map { it.length }.distinct().single()
+
+    val report = reportStrings.map { it.toInt(radix = 2) }
+
+    determinePowerConsumption(report, bitSize)
+    determineLifeSupportRating(report, bitSize)
 }
